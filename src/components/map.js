e7c438400921ef5c08c64bb3421f44bb3e76fc0e
Sammy <o.samy39@gmail.com>
2021-10-14 11:36:39 +0530

    adding markers and tooltips for published locations

--- src/components/map.js
+++ src/components/map.js
@@ -1,15 +1,31 @@
 import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet";
 
-const Map = () =>{
+const Map = ({ locations }) =>{
 
-    return (
-      <MapContainer center={[38.8937, -77.0148]} zoom={11}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
-    );
-  }
-  
-  export default Map
+  return (
+    <MapContainer center={[38.8937, -77.0148]} zoom={11}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {
+        locations.map((loc,index)=>(
+          <Marker
+            key={loc.properties.title+index}
+            position={[
+              loc.geometry.coordinates[1],
+              loc.geometry.coordinates[0]
+            ]}
+          >
+            <Tooltip> 
+              {loc.properties.description} <br />
+              {loc.properties.address}
+            </Tooltip>
+          </Marker>
+        ))
+      }
+    </MapContainer>
+  );
+}
+
+export default Map
